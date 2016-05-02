61d94da1963d9c327529088cbdb9cf6ea8a5a023
Long Tran <long.thanh.tran@gmail.com>
2016-05-02 08:44:18 +0700

    Finalize last step of basic tutorial with Comment components

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
+var fs = require('fs');
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.use(express.static("public"))
+var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('port', (process.env.PORT || 3000))
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(function(req, res, next) {
+  // Set permissive CORS header - this allows this server to be used only as
+  // an API in conjuntion with something like webpack-dev-server.
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
 });
 
+app.get('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.log(err)
+      process.exit(1)
+    }
+    res.json(JSON.parse(data))
+  })
+})
+
+app.post('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+
+    var comments = JSON.parse(data)
+
+    var newComment = {
+      id: Date.now(),
+      author: req.body.author,
+      text: req.body.text
+    }
+    comments.push(newComment)
+    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 2), function(err) {
+      if (err) {
+        console.log(err)
+        process.exit(1)
+      }
+
+      res.json(comments)
+    })
+  })
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,57 @@
+// Default sample for ReactJS
 // ReactDOM.render(
 //   <h1>Hello, React</h1>,
 //   document.getElementById('example');
 // );
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment (with json)"}
-];
+// I itentionally leave those above lines for reference.
+
 // In JSX syntax
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url:      this.props.url,
+      dataType: 'json',
+      cache:    false,
+      success: (data) => {
+        this.setState({data: data});
+      }.bind(this),
+      error: (xhr, status, err) => {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleCommentSubmit: function(comment) {
+    var comments = this.state.data;
+
+    comment.id = Date.now()
+    var newComments = comments.concat([comment])
+    this.setState({data: newComments})
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: comments});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data}/>
-        <CommentForm />
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
       </div>
     );
   }
@@ -37,11 +75,43 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text: ''}
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author ) {
+      return;
+    }
+    // TODO: send request to the server
+    this.props.onCommentSubmit({author: author, text: text});
+    this.setState({author: '', text: ''});
+  },
   render: function() {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something ..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
@@ -55,9 +125,9 @@ var Comment = React.createClass({
   render: function() {
     return (
       <div className="comment">
-        <h2 className="commentAuthor">
+        <h4 className="commentAuthor">
           {this.props.author}
-        </h2>
+        </h4>
         <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     )
@@ -65,6 +135,6 @@ var Comment = React.createClass({
 })
 
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url="/api/comments" pollInterval={2000}/>,
   document.getElementById('content')
 );
